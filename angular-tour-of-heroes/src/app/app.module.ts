af5441b22c37555f0ba17924b95f006030caa0bc
Pengzhi Sun <pengzhisun@live.com>
2017-07-04 09:44:22 +0800

    add ablity to update and save hero

--- angular-tour-of-heroes/src/app/app.module.ts
+++ angular-tour-of-heroes/src/app/app.module.ts
@@ -1,12 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms'
+import { HttpModule } from "@angular/http";
 
 import { AppRoutingModule } from "./modules/app-routing/app-routing.module";
 
 import { AppComponent } from './app.component';
 import { HeroDetailComponent } from './components/hero-detail/hero-detail.component';
 import { HeroesComponent } from './components/heroes/heroes.component';
+
+import { InMemoryWebApiModule } from "angular-in-memory-web-api";
+import { InMemoryDataService } from "./services/in-memory-data.service";
 
 import { HeroService } from "./services/hero.service";
 import { DashboardComponent } from './components/dashboard/dashboard.component';
@@ -21,6 +25,8 @@ import { DashboardComponent } from './components/dashboard/dashboard.component';
   imports: [
     BrowserModule,
     FormsModule,
+    HttpModule,
+    InMemoryWebApiModule.forRoot(InMemoryDataService),
     AppRoutingModule
   ],
   providers: [HeroService],
