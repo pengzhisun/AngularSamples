a1a8fa092d37e8195691f186808a98ede34946dc
Pengzhi Sun <pengzhisun@live.com>
2017-07-05 19:04:58 +0800

    update init bash env scripts

--- angular-tour-of-heroes/init_bash_env.sh
+++ angular-tour-of-heroes/init_bash_env.sh
@@ -1,20 +1,22 @@
-export SRC_ROOT=$(readlink -f "./src/")
+export ROOT_DIR=$(dirname $(readlink -f ${BASH_SOURCE[0]}))
 
-alias env_ver='echo v0.1'
+export SRC_ROOT="$ROOT_DIR/src/"
 
-alias uinit='nano $SRC_ROOT/../init_bash_env.sh'
-alias rewind='pushd . > /dev/null && root && . ./init_bash_env.sh && popd > /dev/null'
+alias env_ver="echo v0.1"
 
-alias root='cd $SRC_ROOT/../'
-alias src='cd $SRC_ROOT'
-alias services='cd $SRC_ROOT/app/services/'
-alias componenets='cd $SRC_ROOT/app/componenets/'
-alias modules='cd $SRC_ROOT/app/modules/'
+alias uinit="nano $ROOT_DIR/init_bash_env.sh"
+alias rewind="pushd . > /dev/null && root && . ./init_bash_env.sh && popd > /dev/null"
 
-alias status='git status'
-alias gpull='git pull'
-alias gpush='git push'
-alias gadda='git add -A'
+alias root="cd $ROOT_DIR/"
+alias src="cd $SRC_ROOT"
+alias services="cd $SRC_ROOT/app/services/"
+alias componenets="cd $SRC_ROOT/app/componenets/"
+alias modules="cd $SRC_ROOT/app/modules/"
 
-alias run='ng serve -o'
-alias ide='code $SRC_ROOT/../'
+alias status="git status"
+alias gpull="git pull"
+alias gpush="git push"
+alias gadda="git add -A"
+
+alias run="ng serve -o"
+alias ide="code $ROOT_DIR/"
